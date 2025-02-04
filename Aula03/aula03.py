c3fd2c5c1edc5464d64cc8f1c89f55fe2bb3739d
matheusgo1 <matheus_go@msn.com>
2025-02-03 19:04:05 -0500

    Aula 03

---
+++ Aula03/aula03.py
@@ -0,0 +1,17 @@
+#quero inserir o nome de uma pessoa na lista pela entrada do usuário
+#quero que a aplicação imprima o tempo de execução
+
+import time
+start_time=time.time()
+lista1=["Domingo", "Terca-Feira", "Quinta-Feira"]
+
+incluir_lista=input("Incluir na lista: ")
+lista1.append(incluir_lista)
+
+end_time=time.time()
+execution_time=end_time-start_time
+
+print(lista1)
+print(f"Tempo de Execucao:  {execution_time:.6f} segundos")
+
+
